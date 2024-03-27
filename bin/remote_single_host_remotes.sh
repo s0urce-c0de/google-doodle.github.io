d126d94ea35e5d22ab091e465d86b9f1a00d5556
s0urce-c0de <email.kanavg@gmail.com>
2024-03-27 14:55:04 -0700

    moved to bin

--- bin/remote_single_host_remotes.sh
+++ bin/remote_single_host_remotes.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+cd "$(dirname "$(dirname "$0")")"
+
 for doodle in $(jq -r '.[]' doodles/doodles.json); do
   git remote remove "$doodle" 
 done
--- bin/single_host.sh
+++ bin/single_host.sh
@@ -2,10 +2,10 @@
 # this is for testing if you don't have a fancy server that can handle
 # multiple paths. These files are excluded in .gitignore
 # you need git subtree
-cd "$(dirname "$0")"
+cd "$(dirname "$(dirname "$0")")"
 
 for doodle in $(jq -r '.[]' doodles/doodles.json); do
-  subtree/add_subtree "$doodle" "https://github.com/Google-Doodle/${doodle}.git" "$doodle" "main" # we always use main
+  bin/subtree/add_subtree "$doodle" "https://github.com/Google-Doodle/${doodle}.git" "$doodle" "main" # we always use main
 done
 
 ALREADY_WRITTEN_FILE=".testing_without_fancy_server"
